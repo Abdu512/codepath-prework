713e07a808af5f991669947a52c6da5b21a51df2
Abdu512 <abdullahbaghdadi@utexas.edu>
2022-08-21 16:06:05 -0500

    Prework for Codepath

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func tipPercentageSelection(_ sender: UISlider) {
+        // Get tip percentage value from slider
+        let tipPercentage = Int(sender.value)
+        tipPercentageLabel.text = "\(tipPercentage)" + "%"
+        
+    }
+    
+    @IBAction func calculateTip(_ sender: UISlider) {
+        // Get bill amount inputted to the text field
+        let bill = Double(billAmountTextField.text!) ?? 0
+            
+        // Calculate total tip (tip * tipPercentage)
+        let tipPercentage = Double(Int(sender.value))/100
+        let tip = bill * tipPercentage
+        let total = bill + tip
+        
+        //Update tip label amount
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
 }
 
